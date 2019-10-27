45138aadc2f30b2db29c9ac1a52a71dae474e283
ym <yangmi0829@vip.qq.com>
2019-10-27 12:08:25 +0800

    自动部署

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果你想要部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# 如果你想要部署到 https://yangmi0829.github.io
+# git push -f git@github.com:yangmi0829/yangmi0829.github.io.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:yangmi0829/blog.git master:gh-pages
